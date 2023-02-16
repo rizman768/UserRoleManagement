8ee940c0894a5179428d164da3cffcef50d9575c
rizman768 <rizqimaulana768@gmail.com>
2023-02-16 15:19:11 +0700

    timestamp

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -14,6 +14,7 @@
     <th>User</th>
     <th>Action</th>
     <th>Resource</th>
+    <th>Timestamps</th>
     </tr>
     @foreach ($data as $key => $log)
     <tr>
@@ -21,6 +22,11 @@
         <td>{{ $log->user }}</td>
         <td>{{ $log->action }}</td>
         <td>{{ $log->resource }}</td>
+        @if($log->action == "Create")
+            <td>{{ $log->created_at }}</td>
+        @else
+            <td>{{ $log->updated_at }}</td>
+        @endif
     </tr>
     @endforeach
     </table>
